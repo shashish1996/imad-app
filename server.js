d551a8a2397dc358ed592378b26c9f312bf6dab4
shashish1996 <36408606+shashish1996@users.noreply.github.com>
2018-02-14 21:14:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/ashish.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ashish.png'));
 });
 
 
