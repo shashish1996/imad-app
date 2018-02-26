acbece1ffe2b9e7a9baccefd12cafaed91bcc0fa
shashish1996 <36408606+shashish1996@users.noreply.github.com>
2018-02-27 00:48:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool= require('pg').Pool;
-
-var config = {
-    user: 'ashishsharma94512',
-    database: 'ashishsharma94512',
-    host: 'db.imad.hasura.app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
-
-
 
 
 var app = express();
@@ -21,65 +10,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
   
-function hash (input, salt){
-    var hashed = crypto.pbkdf25ync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
-}
-
-app.get('/hash/:input',function(req,res){
-    var hashedString=hash(req.params.input,'this-is-some-random-string');
-    res.send(hashedString);
-});
-  
-  
-  
-  
-  
-  
-  
-  
-
-var pool = new Pool(Config);
-  
-app.get('/test-db',function (req,res){
-    //make a select request 
-    // return the response with the results
-    pool.query('SELECT * FROM test', function (err, result){
-        if(err){
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result));
-        }
-    });
-    
-});
- 
-
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/ashish.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ashish.png'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
-app.listen(port, function () {
+var port = 8080;
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
