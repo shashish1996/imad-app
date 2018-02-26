a37524f3c5430b90e3093efafef56f06d1ddc185
shashish1996 <36408606+shashish1996@users.noreply.github.com>
2018-02-27 00:16:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
+  
+function hash (input, salt){
+    var hashed = crypto.pbkdf25ync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+});
+  
+  
+  
+  
+  
+  
+  
+  
 
 var pool = new Pool(Config);
   
